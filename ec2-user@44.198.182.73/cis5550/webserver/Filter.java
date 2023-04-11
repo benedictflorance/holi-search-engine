5fc918763b5816ffbfb24a964a6349594729efbc
lhkung <92461594+lhkung@users.noreply.github.com>
2023-04-11 15:24:15 -0400

    Delete ec2-user@44.198.182.73 directory

--- ec2-user@44.198.182.73/cis5550/webserver/Filter.java
+++
@@ -1,6 +0,0 @@
-package cis5550.webserver;
-
-@FunctionalInterface
-public interface Filter {
-    void handle(Request request, Response response) throws Exception;
-}
--- ec2-user@44.198.182.73/src/cis5550/flame/CustomException.java
+++
@@ -1,9 +0,0 @@
-package cis5550.flame;
-
-public class CustomException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-	public CustomException(String errorMessage) {
-        super(errorMessage);
-    }
-}
