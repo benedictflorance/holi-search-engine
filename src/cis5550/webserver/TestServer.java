3810d8283af1f0437d4f7f56d5557459d014299e
HedgeHog-Head <konsaigao81@gmail.com>
2023-04-11 10:24:01 -0400

    modified frontend interface without react

--- src/cis5550/webserver/TestServer.java
+++ src/cis5550/webserver/TestServer.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cis5550.webserver.Server.staticFiles;
-
+import cis5550.webserver.*;
 
 public class TestServer {
 	public static void main(String args[]) throws Exception {
 		port(8080);
-		staticFiles.location("frontend_interface/search-app/build");
+//		staticFiles.location("frontend_interface/search-app/build");
+		get("/", (req, res) -> {
+			return Interface.homepage;
+		});
+		
 		get("/search", (req, res) -> {
+			
 			String resp = "["
              + "{\"title\": \"title 1\", \"text\": \"http://simple.crawltest.cis5550.net:80/Cv1epgGc.html\"},"
              + "{\"title\": \"title 2\", \"text\": \"http://simple.crawltest.cis5550.net:80/ItU5tEu.html\"},"
@@ -21,6 +26,7 @@ public class TestServer {
 
 			res.header("Content-Type", "text/plain");
 //			res.body(resp);
+
 			res.write(resp.getBytes());
 			return null;
 		});
