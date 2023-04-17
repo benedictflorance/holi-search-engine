47e313c50289ed5648b703fd0be3173dad475b60
lhkung <lhkung@seas.upenn.edu>
2023-04-16 21:52:48 -0400

    Add ignore word list

--- src/cis5550/jobs/Constants.java
+++ src/cis5550/jobs/Constants.java
@@ -2,10 +2,30 @@ package cis5550.jobs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Constants {
 	public static List<String> blacklist = new ArrayList<String>(Arrays.asList(URLExtractor.buildBadURLsList()));
 	public static String CRAWL = "crawl";
 	public static String INDEX = "index";
+	public static Set<String> ignore = new HashSet<String>(Arrays.asList("the",
+																		 "a", 
+																		 "an", 
+																		 "which", 
+																		 "that",
+																		 "and", 
+																		 "or", 
+																		 "for",
+																		 "nor",
+																		 "but",
+																		 "yet",
+																		 "so",
+																		 "that",
+																		 "which",
+																		 "who",
+																		 "as",
+																		 "of"
+																		 ));
 }
