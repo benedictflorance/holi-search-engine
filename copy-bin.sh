0f340e576fd6268b38503c9a4433ee6955112336
lhkung <lhkung@seas.upenn.edu>
2023-04-20 16:46:08 -0400

    shell scripts

--- copy-bin.sh
+++ copy-bin.sh
@@ -1,5 +1,19 @@
 #!/bin/zsh
-scp -i /Users/seankung/upenn/cis555/holi.pem -r /Users/seankung/upenn/cis555/holi-search-engine/bin ec2-user@34.235.126.115:
-scp -i /Users/seankung/upenn/cis555/holi.pem -r /Users/seankung/upenn/cis555/holi-search-engine/bin ec2-user@54.173.64.210:
-scp -i /Users/seankung/upenn/cis555/holi.pem -r /Users/seankung/upenn/cis555/holi-search-engine/bin ec2-user@3.84.206.138:
-scp -i /Users/seankung/upenn/cis555/holi.pem -r /Users/seankung/upenn/cis555/holi-search-engine/bin ec2-user@54.90.122.167:
+# Run inside the project directory of holi-search-engine
+
+pem=holi.pem
+
+node1=34.235.126.115
+node2=54.173.64.210
+node3=3.84.206.138
+node4=54.90.122.167
+
+#scp -i ${pem} -r bin ec2-user@${node1}:
+#scp -i ${pem} -r bin ec2-user@${node2}:
+#scp -i ${pem} -r bin ec2-user@${node3}:
+#scp -i ${pem} -r bin ec2-user@${node4}:
+
+echo ssh -i ${pem} ec2-user@${node1}
+echo ssh -i ${pem} ec2-user@${node2}
+echo ssh -i ${pem} ec2-user@${node3}
+echo ssh -i ${pem} ec2-user@${node4}
