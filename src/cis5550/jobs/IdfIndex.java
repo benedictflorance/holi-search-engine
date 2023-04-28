50ab2be5ac7a2fc7704eadcb799190f845aaed3e
namita-shukla <ns.namitashukla@gmail.com>
2023-04-28 14:36:34 -0400

    Add new idf

---
+++ src/cis5550/jobs/IdfIndex.java
@@ -0,0 +1,34 @@
+package cis5550.jobs;
+
+import static cis5550.kvs.Row.readFrom;
+import java.io.RandomAccessFile;
+import cis5550.kvs.Row;
+
+public class IdfIndex {
+	
+	public static void main(String args[]) {
+		 try {
+			 final Integer N =462821;
+			 RandomAccessFile index = new RandomAccessFile("/Users/namitashukla/Desktop/indexfilter.table", "rw");
+			 RandomAccessFile file = new RandomAccessFile("/Users/namitashukla/Desktop/w-metrics.table", "rw");
+	         while(true){
+	             Row row = readFrom(file);
+	             String urls = row.get("url");
+	             if(urls == null)
+	                continue;
+	             String rowkey = row.key();
+	             Integer df = urls.split(",").length + 1;
+		         Double idf = Math.log(1.0 * N / df);
+		         Row newRow = new Row(rowkey);
+		         newRow.put("df",String.valueOf(df));
+		         newRow.put("idf",String.valueOf(idf));
+	             file.write(newRow.toByteArray());
+	             file.writeBytes("\n");
+	         }
+		 }
+		 catch(Exception e) {
+			 e.printStackTrace();
+		 }
+
+	}
+}
